8ae3f0e9d09c65d9c66fbf1fbb25ac148fca6495
cauaameno <106753236+cauaameno@users.noreply.github.com>
2023-12-01 10:17:34 -0300

    save '

---
+++ main.js
@@ -0,0 +1,17 @@
+function toca Som (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+    }
+    const listaDeTeclas = document.querySelectorAll(".tecla")
+    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas [contador];
+    
+    const efeito = tecla.classList[1];
+    
+    const idAudio = `#som ${efeito}`;
+    
+    tecla.onclick = function() {
+    
+    tocaSom (idAudio);
+    };
+    }
